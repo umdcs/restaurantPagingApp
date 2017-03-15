79524e638c657fdd1f32c41d75550fb5f945453d
Tina Larsen <larse738@d.umn.edu>
2017-03-15 17:18:52 -0500

    Created a basic reservation class with two constructors, data fields for name, party size and phone number, as well as getters and setters for each data field.

---
+++ Android/RestaurantPagingApp/app/src/main/java/edu/umn/d/restaurantpagingapp/Reservation.java
@@ -0,0 +1,46 @@
+package edu.umn.d.restaurantpagingapp;
+
+/**
+ * Created by tinar on 3/15/2017.
+ */
+
+public class Reservation {
+
+    public Reservation(){
+        
+    }
+
+    public Reservation(String name, int partySize, int phoneNumber){
+        this.name = name;
+        this.partySize = partySize;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setPartySize(int partySize){
+        this.partySize = partySize;
+    }
+
+    public int getPartySize(){
+        return partySize;
+    }
+
+    public void setPhoneNumber(int phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    private String name;
+    private int partySize;
+    private int phoneNumber;
+}
