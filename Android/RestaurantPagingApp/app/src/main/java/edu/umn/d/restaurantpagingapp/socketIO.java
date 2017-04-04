2a1479cbf591d49155009c8228b7acb0d746433b
Dana Williams <will4385@d.umn.edu>
2017-04-04 12:18:58 -0500

    Creating socket.io file.

--- Android/RestaurantPagingApp/app/src/main/java/edu/umn/d/restaurantpagingapp/socketIO.java
+++ Android/RestaurantPagingApp/app/src/main/java/edu/umn/d/restaurantpagingapp/socketIO.java
@@ -1,6 +1,36 @@
 package edu.umn.d.restaurantpagingapp;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import io.socket.emitter.Emitter;
+import io.socket.hasbinary.HasBinary;
+import io.socket.parser.Packet;
+import io.socket.parser.Parser;
+import io.socket.thread.EventThread;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.String;
+
+
+/** Socket Class for Client */
+
+public class extends Emitter {
+        private static final Logger logger = Logger.getLogger(Socket.class.getName());
+
+        /** Called on Connection */
+        public static final String EVENT_CONNECT = "connect";
+
+        public static final String EVENT_CONNECTING = "connecting";
+
+        /** Called on disconnection */
+        public static final String EVENT_DISCONNECT = "disconnect";
+
+        /** Connection errors */
+
+        public static final String EVENT_ERROR = "error";
+        public static final String EVENT_MESSAGE = "message";
+        }
 
